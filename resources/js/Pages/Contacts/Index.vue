1479fd5485c4ea7eb4f69dc74db2b0c1b4a47769
vaska <vaska.r02@gmail.com>
2022-01-24 14:35:32 +0000

    wup

--- resources/js/Pages/Contacts/Index.vue
+++ resources/js/Pages/Contacts/Index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <Head title="Contacts" />
+    <Head title="Contacts"/>
     <h1 class="mb-8 text-3xl font-bold">Contacts</h1>
     <div class="flex items-center justify-between mb-6">
       <search-filter v-model="form.search" class="mr-4 w-full max-w-md" @reset="reset">
         <label class="block text-gray-700">Trashed:</label>
         <select v-model="form.trashed" class="form-select mt-1 w-full">
-          <option :value="null" />
+          <option :value="null"/>
           <option value="with">With Trashed</option>
           <option value="only">Only Trashed</option>
         </select>
@@ -28,7 +28,7 @@
           <td class="border-t">
             <Link class="flex items-center px-6 py-4 focus:text-indigo-500" :href="`/contacts/${contact.id}/edit`">
               {{ contact.name }}
-              <icon v-if="contact.deleted_at" name="trash" class="flex-shrink-0 ml-2 w-3 h-3 fill-gray-400" />
+              <icon v-if="contact.deleted_at" name="trash" class="flex-shrink-0 ml-2 w-3 h-3 fill-gray-400"/>
             </Link>
           </td>
           <td class="border-t">
@@ -50,7 +50,7 @@
           </td>
           <td class="w-px border-t">
             <Link class="flex items-center px-4" :href="`/contacts/${contact.id}/edit`" tabindex="-1">
-              <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400" />
+              <icon name="cheveron-right" class="block w-6 h-6 fill-gray-400"/>
             </Link>
           </td>
         </tr>
@@ -59,12 +59,12 @@
         </tr>
       </table>
     </div>
-    <pagination class="mt-6" :links="contacts.links" />
+    <pagination class="mt-6" :links="contacts.links"/>
   </div>
 </template>
 
 <script>
-import { Head, Link } from '@inertiajs/inertia-vue3'
+import {Head, Link} from '@inertiajs/inertia-vue3'
 import Icon from '@/Shared/Icon'
 import pickBy from 'lodash/pickBy'
 import Layout from '@/Shared/Layout'
@@ -98,7 +98,7 @@ export default {
     form: {
       deep: true,
       handler: throttle(function () {
-        this.$inertia.get('/contacts', pickBy(this.form), { preserveState: true })
+        this.$inertia.get('/contacts', pickBy(this.form), {preserveState: true})
       }, 150),
     },
   },
